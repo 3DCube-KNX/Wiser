f8e242d16f7430d5b4c7b2d377605219ef47eb02
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-04-27 13:43:14 +0200

    Rename Script Lua Comunicación RS232.lua to lua/Script Lua Comunicación RS232.lua

--- lua/Script Lua Comunicación RS232.lua
+++ lua/Script Lua Comunicación RS232.lua
@@ -1,13 +1,13 @@
 -- Se obtiene el valor del objeto de escenas y se guarda en la variable value
 value = event.getvalue()
 
--- si el valor de la variable value es igual a 3 (número de escena), entonces enviamos el comando de encendido
+-- si el valor de la variable value es igual a 3 (nÃºmero de escena), entonces enviamos el comando de encendido
 if (value == 3) then
   
-  -- Es necesario incluir la librería de las funciones de comunicación serie
+  -- Es necesario incluir la librerÃ­a de las funciones de comunicaciÃ³n serie
     if not port then 
     require('serial')
-    -- Configuración y apertura del puerto serie
+    -- ConfiguraciÃ³n y apertura del puerto serie
     port = serial.open('/dev/RS232', {
   baudrate = 9600,
   databits = 8,
@@ -15,7 +15,7 @@ if (value == 3) then
   parity = 'none',
   duplex = 'full'
     })
-    --  Limpiar bytes no leídos o enviados
+    --  Limpiar bytes no leÃ­dos o enviados
     port:flush()  
     -- Escribir datos en el puerto serie para encender la TV
     msg = string.char(0x6B, 0x61, 0x20, 0x30, 0x31, 0x20, 0x30, 0x31, 0x0D)
