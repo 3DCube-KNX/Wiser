0939ec2fceb6233d187db9ea2473f10c40929753
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-04-27 13:45:14 +0200

    Rename Script Lua Contador descendente.lua to Lua/Script Lua Contador descendente.lua

--- Lua/Script Lua Contador descendente.lua
+++ Lua/Script Lua Contador descendente.lua
@@ -1,4 +1,4 @@
--- Obtenemos el valor de la direcciÛn 1/0/1 y se asigna a la variable
+-- Obtenemos el valor de la direcci√≥n 1/0/1 y se asigna a la variable
 value_1 = grp.getvalue('1/0/1')
 
 if value_1 == true then			-- Si el valor de la variable es 1 entonces 
@@ -8,7 +8,7 @@ Stepper_Value = storage.get('Value_Stepper_1')
 Stepper_Value = 0				-- Inicializamos la variable a 0
 end
 
--- Si el valor de la variable alcanza el mÌnimo valor, entonces no lo decrementamos m·s	
+-- Si el valor de la variable alcanza el m√≠nimo valor, entonces no lo decrementamos m√°s	
   if Stepper_Value == 0 then
 else 							-- De lo contrario,		
 -- Decrementamos en 1 la variable que almacena el valor actual	
@@ -17,6 +17,6 @@ Stepper_Value = Stepper_Value - 1
   
 -- Guardamos en el registro el valor de la variable
   storage.set('Value_Stepper_1', Stepper_Value)
--- Se escribe en la direcciÛn de grupo el valor de la variable (total contador)
+-- Se escribe en la direcci√≥n de grupo el valor de la variable (total contador)
   grp.write('1/0/6', Stepper_Value)						
 end 
