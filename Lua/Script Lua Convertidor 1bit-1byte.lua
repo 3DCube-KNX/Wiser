c90e4d0ff9b361792a93dc88ec6fd30eada183ea
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-05-05 11:01:51 +0200

    Rename Script Lua Convertidor 1bit-1byte.lua to Lua/Script Lua Convertidor 1bit-1byte.lua

--- Lua/Script Lua Convertidor 1bit-1byte.lua
+++ Lua/Script Lua Convertidor 1bit-1byte.lua
@@ -2,8 +2,8 @@
 value = event.getvalue()	-- El valor del evento se guarda en la variable value
 
 if value == true then		-- Si la variable valor es igual a 1 (true), entonces
-grp.write('1/0/6', 100)		-- Se escribe el valor 100 en la dirección de grupo de salida de 1 byte
+grp.write('1/0/6', 100)		-- Se escribe el valor 100 en la direcciÃ³n de grupo de salida de 1 byte
 else						-- De lo contrario, es decir si es 0 (false)
-grp.write('1/0/6', 0)		-- Se escribe el valor 0 en la dirección de grupo de salida de 1 byte
+grp.write('1/0/6', 0)		-- Se escribe el valor 0 en la direcciÃ³n de grupo de salida de 1 byte
 end
 
