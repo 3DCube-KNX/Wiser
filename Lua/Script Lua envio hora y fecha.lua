727ad80f782d284a1ef57fd2cf382096bd199ad0
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-05-05 11:05:09 +0200

    Rename Script Lua envio hora y fecha.lua to Lua/Script Lua envio hora y fecha.lua

--- Lua/Script Lua envio hora y fecha.lua
+++ Lua/Script Lua envio hora y fecha.lua
@@ -1,7 +1,7 @@
 --Primero obtener la hora como una tabla y asignala a la variable now
     now = os.date('*t')
      
--- El día de la semana empieza en domingo y he de convertirlo al formato KNX que empieza en lunes
+-- El dÃ­a de la semana empieza en domingo y he de convertirlo al formato KNX que empieza en lunes
     wday = now.wday == 1 and 7 or now.wday - 1
 -- Tabla de hora en la variable time
     time = { 
@@ -17,7 +17,7 @@
     month = now.month,
     year = now.year,
     }
--- actualizar el valor de la dirección de grupo, con la tabla time, dt.time y dt.date son los DPTs correspondientes 
+-- actualizar el valor de la direcciÃ³n de grupo, con la tabla time, dt.time y dt.date son los DPTs correspondientes 
     grp.update('1/2/14', time, dt.time)
     grp.update('1/2/15', date, dt.date)
   
