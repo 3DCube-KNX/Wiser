a7a49d8173e23b42642fc283f3ca02dc97e961d1
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-05-05 11:02:07 +0200

    Rename Script Lua Evento para notificaciones.lua to Lua/Script Lua Evento para notificaciones.lua

--- Lua/Script Lua Evento para notificaciones.lua
+++ Lua/Script Lua Evento para notificaciones.lua
@@ -1,8 +1,8 @@
-if  value == true then -- Si el valor de la DG es igual a verdadero (1), entonces enviamos la notificaciÛn.
+if  value == true then -- Si el valor de la DG es igual a verdadero (1), entonces enviamos la notificaci√≥n.
 require("user.pushover")
-title = 'Wiser' --TÌtulo del mensaje
+title = 'Wiser' --T√≠tulo del mensaje
 message = 'Alarma de viento'--Mensaje a enviar
---Ajustar el sonido que se escuchar· en el dispositivo
+--Ajustar el sonido que se escuchar√° en el dispositivo
 --pushover(default), bike, bugle, cashregister, classical, cosmic, falling, gamelan, incoming, intermission, magic, mechanical, pianobar, siren, spacealarm, tugboat, alien, climb, persistent, echo, updown, none
 sound = 'updown'
 --Definir la prioridad
@@ -10,8 +10,8 @@ sound = 'updown'
 priority = 2 -- Prioridad se salta el silencio de la app por parte del usuario y requiere acuse de recibo
 --Segundos para reintentar cuando no se acusa el recibo, minino 30(solo en prioridad 2)
 retry = 30 
---Segundos expiraciÛn reintento si no hay acuse de recibo, m·ximo = 86400 (24 hrs, solo en prioridad 2)
+--Segundos expiraci√≥n reintento si no hay acuse de recibo, m√°ximo = 86400 (24 hrs, solo en prioridad 2)
 expire = 3600 
---EnvÌo de la notificaciÛn con los par·metros
+--Env√≠o de la notificaci√≥n con los par√°metros
 pushover(title, message, sound, priority, retry, expire)
 end
