009cb62cd2e864d94b299d70a2b404b0d8d9cdf8
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-05-05 11:05:30 +0200

    Rename Script Lua evento telegram.lua to Lua/Script Lua evento telegram.lua

--- Lua/Script Lua evento telegram.lua
+++ Lua/Script Lua evento telegram.lua
@@ -1,4 +1,4 @@
-require("user.telegram") 		-- Requiere la librería de usuario
+require("user.telegram") 		-- Requiere la librerÃ­a de usuario
 message = 'test message'		-- Texto del mensaje a enviar
 res, err = telegram(message) 	-- Se pasa a la biblioteca de usuario el mensaje
 log(res, err)
