83d88f3967c1efdb7362ca8d76e9642a16adc48f
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-06-17 17:42:36 +0200

    Add files via upload

--- Lua/Script Lua PVPC.lua
+++ Lua/Script Lua PVPC.lua
@@ -5,26 +5,26 @@ ltn12 = require('ltn12')
 
 payload =  ''
 
-token ='su token' 
+token ='4ee20705e6b7c6b05a872d5171a3ba3be1d1936835de7c1722aa02da99356b2a' 
 Token=('Token token=\34'..token..'\34')
 
 -- Obtención de la fecha para la petición
-tom = os.time(os.date('*t')) + 86400 	--Hoy más 86400 segundos (+1 día)
-tomorrow = os.date("*t", tom)
-day = tomorrow.day
-month = tomorrow.month
-year = tomorrow.year
+mañana_sin_formato = os.time(os.date('*t')) --+ 86400 	--Hoy más 86400 segundos (+1 día)
+mañana_con_formato = os.date("*t", mañana_sin_formato)
+dia = mañana_con_formato.day
+mes = mañana_con_formato.month
+año = mañana_con_formato.year
 
 
--- Url donde está el PVPC con su correspondiente indicador 1001 PVPC T 2.0D
-url = 'https://api.esios.ree.es/indicators/1001?start_date='..day..'-'..month..'-'..year..'T00%3A00&end_date='..day..'-'..month..'-'..year..'T23%3A50' --Término de facturación de energía activa del PVPC
+-- Url donde está el PVPC 2.0TD con su correspondiente indicador 1001 y filtro para solo datos peninsulares
+url = 'https://api.esios.ree.es/indicators/1001?start_date='..dia..'-'..mes..'-'..año..'T00%3A00Z&end_date='..dia..'-'..mes..'-'..año..'T23%3A50Z&geo_ids[]=8741'
 
 -- Petición
 response_body = {}
-  
-body, code, hdrs, stat = https.request ({
+
+body, code, hdrs, stat = https.request ({ --Llamada HTTP formada por URL+metodo+cabecera
 url = url;
-method = 'GET';
+  method = 'GET';
 headers = {
         ['Accept'] = 'application/json; application/vnd.esios-api-v1+json'; 
         ['Content-Type'] = 'application/json';
@@ -37,26 +37,27 @@ headers = {
 })
 
 -- Comprobación que la recepción en correcta
-if type(response_body) ~= 'table' then												-- Si el contenido de la variable es distinto a una tabla entonces
+if type(response_body) ~= 'table' then															-- Si el contenido de la variable es distinto a una tabla entonces
   alert('Fallo al cargar la información')														
-elseif code == 200 then																	-- Si la llamada devuelve el codigo 200, todo OK
+elseif code == 200 then																			-- Si la llamada devuelve el codigo 200, todo OK
   log(code .. ' Datos obtenidos con éxito')
-elseif code == 401 then																	-- Si la llamada devuelve el codigo de error 401 o 404																													
-  alert(code .. ' Fallo con la llave de la API')										-- Mensajes de error
+elseif code == 401 then																			-- Si la llamada devuelve el codigo de error 401 o 404																													
+  alert(code .. ' Fallo con la llave de la API')												-- Mensajes de error
 elseif code == 404 then
   alert(code .. ' Fallo datos no encontrados')
-  
   return
 end
 
 -- Tratamiento de los datos recibidos uniéndolos en una sola cadena y decodificando el Json
 response = table.concat(response_body)
-data = require('json').decode(response)
 
+data = json.pdecode(response)
+log(data)
 --Utilizamos los datos de la PVCP obtenidos, se divide entre 1000 para obtener kWh--
 --Hora 00
-grp.write('32/2/21', data.indicator.values[ 1 ].value/1000)
+grp.write('32/2/21', data.indicator.values[ 23 ].value/1000)
 
 
 --Hora 01
-grp.write('32/2/22', data.indicator.values[ 2 ].value/1000)
+grp.write('32/2/22', data.indicator.values[ 24 ].value/1000)
+
