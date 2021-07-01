a5bf95e7dbdd138eeea0941a2e4acae48a3e5e67
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2021-07-01 20:04:58 +0200

    Add files via upload

---
+++ Javascript/Javascript página por valor.js
@@ -0,0 +1,18 @@
+$(function(){
+  if (typeof grp != 'undefined') {
+    grp.listen('32/2/46', function(object, state) { 
+      var value = object.value; //Declaracion  de la variable y asignacion del valor del objeto
+      if (state == 'value') {
+        if (value == 1) { 	//si el valor del objeto es 1
+          showPlan(3);		// Muestra el plan indicado entre los parentesis
+        }
+        else if (value == 2) {
+          showPlan(4);
+        }
+        else if (value == 3) {
+          showPlan(12);
+        }
+      }
+    });
+  }
+});
