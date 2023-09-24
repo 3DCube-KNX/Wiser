f2391670296e93e3bfcdc99b39b0c091bf24a4c6
3DCube-KNX <83236680+3DCube-KNX@users.noreply.github.com>
2023-09-24 15:03:22 +0200

    Add files via upload

--- Lua/Script Lua PVPC.lua
+++ Lua/Script Lua PVPC.lua
@@ -5,11 +5,11 @@ ltn12 = require('ltn12')
 
 payload =  ''
 
-token ='4ee20705e6b7c6b05a872d5171a3ba3be1d1936835de7c1722aa02da99356b2a' 
+token ='Escriba aquí su token' 
 Token=('Token token=\34'..token..'\34')
 
 -- Obtención de la fecha para la petición
-mañana_sin_formato = os.time(os.date('*t')) + 86400 	--Hoy más 86400 segundos (+1 día)
+mañana_sin_formato = os.time(os.date('*t')) --+ 86400 	--Hoy más 86400 segundos (+1 día)
 mañana_con_formato = os.date("*t", mañana_sin_formato)
 dia = mañana_con_formato.day
 mes = mañana_con_formato.month
@@ -23,15 +23,14 @@ url = 'https://api.esios.ree.es/indicators/1001?start_date='..dia..'-'..mes..'-'
 response_body = {}
 
 body, code, hdrs, stat = https.request ({ --Llamada HTTP formada por URL+metodo+cabecera
-url = url;
+  url = url;
   method = 'GET';
-headers = {
+  headers = {
         ['Accept'] = 'application/json; application/vnd.esios-api-v1+json'; 
         ['Content-Type'] = 'application/json';
-        ['Host'] = 'api.esios.ree.es';
-        ['Authorization'] = Token;
-        ['Cookie'] = '';
-};
+        ['x-api-key'] = token;
+
+  };
     source = ltn12.source.string(payload);
     sink = ltn12.sink.table(response_body);
 })
@@ -54,16 +53,46 @@ response = table.concat(response_body)
 data = json.pdecode(response)
 log(data)
 
---Hora de actualización de los datos 
+--Hora de actualizacion de los datos 
 año, mes, dia, hora, minutos, segundos = string.match(data.indicator.values_updated_at, '(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)')
 fecha_hora_ordenada = string.format('%s-%s-%s, %s:%s:%s', dia, mes, año, hora, minutos, segundos)
+log(fecha_hora_ordenada)
+
 grp.write('32/2/21', fecha_hora_ordenada)
 
 --Utilizamos los datos de la PVCP obtenidos, se divide entre 1000 para obtener kWh--
 --Hora 00
-grp.write('32/2/22', data.indicator.values[1].value/1000)
+grp.write('32/2/22', data.indicator.values[1].value/10)
 
 
 --Hora 01
 grp.write('32/2/23', data.indicator.values[2].value/1000)
+
+-- Hora 24
+
+-- Valor precio:hora más alto y más bajo
+
+values = data.indicator.values -- Guardamos, en una variable, la tabla con solo los valores, esto descarta el resto de la información
+log(values)
+
+storage.set('pvpc', values)   -- 
+
+table.sort(values, function(a, b)  -- Se ordena la tabla que contiene los valores de menor valor a máyor valor de precio y se guarda ordenada en value
+  return a.value < b.value
+end)
+
+min = values[1]                   			-- Se guarda la primera posición de la tabla en la variable min, es una tabla con el precio y la hora
+minvalue = min.value/1000               -- Se guarda en la variable, la clave value de la tabla min y se divide entre mill para los kWh. Este es el precio minimo
+minhour = min.datetime:match('T(%d+)')  -- Se guarda en la variable, la clave datetime de la tabla min y se quita todo lo que nosea la hora. Esta es la hora
+
+log(minvalue, minhour)
+
+max = values[#values]
+log(max)
+maxvalue = max.value/1000
+maxhour = max.datetime:match('T(%d+)')
+
+log(maxvalue, maxhour)
+
+
 
